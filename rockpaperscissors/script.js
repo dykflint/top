8c2c90227c43b3014c3e715abb6f2868de222f20
DykFlint <konstantin.tamoev@gmail.com>
2023-04-13 18:57:14 +0200

    Create rps ui

--- rockpaperscissors/script.js
+++ rockpaperscissors/script.js
@@ -1,14 +1,29 @@
 // ROCK PAPER SCISSORS
 
-let choices = ["Rock", "Paper", "Scissors"]
+let choices = ["rock", "paper", "scissors"]
+const computerChoiceButton = document.querySelector('#computer-choice');
+const computerChoiceImg = document.querySelector('#computer-choice-img');
+const playerRockButton = document.querySelector('.rock');
+const playerPaperButton = document.querySelector('.paper');
+const playerScissorsButton = document.querySelector('.scissors');
+const scoreContainer = document.querySelector('.score');
+let playerButtons = [playerRockButton, playerPaperButton, playerScissorsButton];
 // let playerSelection = "rock";
 let playerSelection;
 let computerSelection;
 let computerWinCount = 0;
 let playerWinCount = 0;
 
+// Show choices again and remove previous computer choice 
+function showChoicesAgain() {
+    playerButtons.forEach(function(item){
+        item.classList.remove("hide");
+    })
+    computerChoiceImg.src = "";
+}
 function getComputerChoice() {
     let randomComputerChoice = Math.floor(Math.random()*2);
+    computerChoiceImg.src = "img/" + randomComputerChoice + ".png";
     return choices[randomComputerChoice];
 }
 
@@ -52,22 +67,27 @@ function playRound(playerSelection, computerSelection){
     }
 }
 
-function game(){
-    while(playerWinCount + computerWinCount != 5){
-        playerSelection = prompt("What's your move?");
+playerButtons.forEach(function(item) {
+    item.addEventListener('click', () => {
+        playerRockButton.classList.add('hide');
+        playerPaperButton.classList.add('hide');
+        playerScissorsButton.classList.add('hide');
+        item.classList.remove('hide');
+        playerSelection = item.classList[0];
+        console.log(playerSelection);
         computerSelection = getComputerChoice();
-        console.log("You picked: " + playerSelection);
-        console.log("Your opponent picked: " + computerSelection);
+        computerChoiceImg.src = "img/" + computerSelection + ".png";
         console.log(playRound(playerSelection, computerSelection));
-    }
-    if(playerWinCount > computerWinCount){
-        console.log("You won " + playerWinCount + " out of 5 Rounds. Congratulations. Reload for a new game.");
-    } else {
-        console.log("You only won " + playerWinCount + " out of 5 Rounds. While your opponent won " + computerWinCount + " Rounds. Reload for a new game.")
-    }
+        scoreContainer.innerText = String(playerWinCount) + "/" + String(playerWinCount + computerWinCount);
+        setTimeout(showChoicesAgain, 1000);
+        console.log(playerWinCount);
+        if(playerWinCount + computerWinCount == 5){
+            if(playerWinCount > computerWinCount){
+                scoreContainer.innerText = "You won " + playerWinCount + " out of 5 Rounds. Congratulations. Reload for a new game.";
+            } else {
+                scoreContainer.innerText = "You only won " + playerWinCount + " out of 5 Rounds. While your opponent won " + computerWinCount + " Rounds. Reload for a new game.";
+            }
+        }
+    });
+})
 
-}
-
-
-// PLAY 5 ROUNDS
-game();
