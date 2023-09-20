0d86335148a8a19b0a3ae906fbb6730305e93aec
DykFlint <konstantin.tamoev@gmail.com>
2023-09-20 13:08:29 +0200

    Added the clear and dot functionality

--- calculator/script.js
+++ calculator/script.js
@@ -1,8 +1,8 @@
 const resultDiv = document.querySelector('#result');
 const operators = Array.from(document.querySelector('#operators').children);
 const basicOperations = ['+','-','*','/'];
+const equal = document.querySelector('#equal');
 let allNumRows = document.querySelectorAll('.row');
-
 function showNumber(num) {
   resultDiv.textContent += num;
 }
@@ -18,29 +18,42 @@ allNumRows.forEach(parElem => {
 })
 operators.forEach(operator => {
   operator.addEventListener('click', () => {
-    let currentInput = resultDiv.textContent;
-    // Remove any pre-existing active operators
-    operators.forEach((element) => {
-      element.classList.remove('activeOperator');
-    })
-    // Add activeOperator status to the newly clicked operator
-    operator.classList.add('activeOperator');
-    // Add operator to result container
-    if(basicOperations.includes(operator.textContent) 
+    if(resultDiv.textContent != ''){
+      let currentInput = resultDiv.textContent;
+      // Remove any pre-existing active operators
+      operators.forEach((element) => {
+       element.classList.remove('activeOperator');
+      })
+      // Add activeOperator status to the newly clicked operator
+      operator.classList.add('activeOperator');
+      // Add operator to result container
+      if(basicOperations.includes(operator.textContent) 
       && !basicOperations.includes(currentInput[currentInput.length - 1])) {
-      resultDiv.textContent += operator.textContent;
-    } else if (basicOperations.includes(currentInput[currentInput.length - 1])) {
-      currentInputArr = currentInput.split('');
-      currentInputArr[currentInput.length - 1] = operator.textContent;
-      resultDiv.textContent = currentInputArr.join('');
+        resultDiv.textContent += operator.textContent;
+      } else if (basicOperations.includes(currentInput[currentInput.length - 1]) 
+        && operator.textContent != '=') {
+        currentInputArr = currentInput.split('');
+        currentInputArr[currentInput.length - 1] = operator.textContent;
+        resultDiv.textContent = currentInputArr.join('');
+      }
     }
-  })
-})
+  });
+});
 
 function calculate() {
+  equal.classList.remove('activeOperator');
   resultDiv.textContent = eval(resultDiv.textContent);
 }
 
-function clear() {
+function addDot() {
+  let splitEquation = resultDiv.textContent.split(/[+\-*/]/);
+  if(!splitEquation[splitEquation.length - 1].includes('.')) resultDiv.textContent += '.';
+}
+
+function clearLog() {
+  // Remove active operators 
+  operators.forEach(operator => {
+    operator.classList.remove('activeOperator');
+  })
   resultDiv.textContent = '';
 }
